8a9331d44c2578824cb6d31b849dbaa15589f2dd
TacoWithaTwist <houssein.msallem@gmail.com>
2023-12-03 12:48:51 +0100

    got the functionality of the library working

--- app.js
+++ app.js
@@ -2,36 +2,51 @@ var button = document.getElementById("newBook")
 var displayButton = document.getElementById("displayBook")
 button.addEventListener('click',addBookToLibrary)
 displayButton.addEventListener('click',displayBook)
-const myLibrary = [{title:'houssein',author:'yes',pages:'100',read:'read'}]
+const myLibrary = [{title:'How to Raise your BABY',author:'Angela Merkel',pages:'100',status:'unread'}]
 
-//Book constructor
 
 function Book(title,author,pages,read){
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = read
-    this.info = function(){
-        return(this.title + this.author + this.pages + this.read)
-    }
+    this.status = read
 }
 
-//Add new book to Library
 
 function addBookToLibrary(){
-    const inputTitle = prompt("Give new book title")
-    const inputAuthor = prompt("Give new book author")
-    const inputPages = prompt("Give new book pages")
-    const inputRead = prompt("Give new book read or not")
-    const newBook = new Book (inputTitle,inputAuthor,inputPages,inputRead)
-    console.log(newBook.info())
+    const title = prompt("Give new book title")
+    const author = prompt("Give new book author")
+    const pages = prompt("Give new book pages")
+    const status = prompt("Give new book read or not")
+    const newBook = new Book (title,author,pages,status)
+    myLibrary.push(newBook)
+    console.log(myLibrary)
 
 }
 
-//Display books in library
-
 function displayBook(){
+    const container = document.getElementById("container")
+    const getCard = document.getElementById("cards")
+    while(getCard){
+        const getCard = document.getElementById("cards")
+        if(!getCard){
+            break
+        }
+        container.removeChild(getCard)
+    }
     myLibrary.forEach(book => {
-        console.log(book)
+            const card = document.createElement("div")
+            card.className = "cards"
+            card.id = "cards"
+            container.appendChild(card);
+            var ul = document.createElement("ul");
+            ul.className = "card-text"
+            card.appendChild(ul)
+            for (var key in book){
+                var li = document.createElement("li");
+                li.className = "card-text"
+                li.innerHTML = `<strong>${key}:</strong> ${book[key]}`;
+                ul.appendChild(li)
+            }
     });
 }
